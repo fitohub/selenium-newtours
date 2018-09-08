ed1451812ad41cec38d90f03d4a625a90d908817
fernandoito <fernando.ito.anywhere@gmail.com>
2018-09-07 22:05:20 -0400

    Flight Finder page completed

--- src/main/java/newTours.java
+++ src/main/java/newTours.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.TimeoutException;
 
@@ -19,6 +20,7 @@ public class newTours {
         // And now use this to visit Mercury Tours website
         driver.get("http://newtours.demoaut.com");
 
+        // --- Home ---
         // Wait Home page appears
         WebDriverWait wait = new WebDriverWait(driver, 10);
 
@@ -33,6 +35,45 @@ public class newTours {
 
         // Wait Flight Finder page appears
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("findFlights")));
+
+
+        // --- Flight Finder - Flight Details ---
+
+        // Select Quantity of Passengers
+        new Select(driver.findElement(By.name("passCount"))).selectByVisibleText("2");
+
+
+        // Select Departure City
+        new Select(driver.findElement(By.name("fromPort"))).selectByVisibleText("New York");
+
+
+        // Select Departure Month
+        new Select(driver.findElement(By.name("fromMonth"))).selectByVisibleText("September");
+
+        // Select Departure Day
+        new Select(driver.findElement(By.name("fromDay"))).selectByVisibleText("30");
+
+        // Select Arrival City
+        new Select(driver.findElement(By.name("toPort"))).selectByVisibleText("Paris");
+
+        // Select Arrival Month
+        new Select(driver.findElement(By.name("toMonth"))).selectByVisibleText("October");
+
+        // Select Arrival Day
+        new Select(driver.findElement(By.name("toDay"))).selectByVisibleText("25");
+
+        // Set Service Class
+        driver.findElement(By.cssSelector("input[value='First']")).click();
+
+        // Select Airline
+        new Select(driver.findElement(By.name("airline"))).selectByVisibleText("Unified Airlines");
+
+        // Click Continue button
+        driver.findElement(By.name("findFlights")).click();
+
+        // Wait SELECT FLIGHT page appears
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("reserveFlights")));
+
 
         // Close the browser
         //driver.quit();
