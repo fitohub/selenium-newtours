c019dbfc46b18fc119c12d62a02308ccb2f1f95c
fernandoito <fernando.ito.anywhere@gmail.com>
2018-09-12 17:19:04 -0400

    Experience with data-driven with Apache POI and jxl

--- src/test/java/tests/newTours.java
+++ src/test/java/tests/newTours.java
@@ -10,13 +10,11 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import java.io.IOException;
 
 public class newTours {
 
-    // Global test data Excel file
-    public static final String testDataExcelFileName = "testdata.xlsx";
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         // Set the property for webdriver.chrome.driver to be the location to your local download of chromeDriver
         System.setProperty("webdriver.chrome.driver", "/Users/fernandoito/selenium/chromeDriver");
@@ -31,6 +29,18 @@ public class newTours {
 
         // Wait Home page appears
         WebDriverWait wait = new WebDriverWait(driver, 10);
+
+        // Create an object of HomeTest class
+        HomeTest objExcelFile = new HomeTest();
+
+        // Prepare the path of Excel file
+
+        String filePath = System.getProperty("user.dir") + "/src/test/java/resources";
+        String fileName = "testdata.xlsx";
+        String sheetName = "HomeData";
+
+        // Call read file method of the class to read data
+        objExcelFile.readExcel(filePath, fileName, sheetName);
 
         HomeTest homeTest = new HomeTest();
 
@@ -117,5 +127,62 @@ public class newTours {
         driver.quit();
 
     }
+/*
+    public static void readExcel(String filePath, String fileName, String sheetName) throws IOException {
+
+        // Create an object of File class to open xlsx file
+        File file = new File(filePath + "/" + fileName);
+
+        // Create an object of FileInputStream class to read excel file
+        FileInputStream inputStream = new FileInputStream(file);
+
+        Workbook loginWorkbook = null;
+
+        // Find the file extension by splitting file name in substring and getting only extension name
+
+        String fileExtensionName = fileName.substring(fileName.indexOf("."));
+
+        // Check condition if the file is xlsx file
+
+        if (fileExtensionName.equals(".xlsx")) {
+
+            // If it is xlsx file then create object of XSSFWorkbook class
+            loginWorkbook = new XSSFWorkbook(inputStream);
+
+        } else {
+
+            // if it is xls file then create object of HSSFWorkbook class
+            loginWorkbook = new HSSFWorkbook(inputStream);
+
+        }
+
+        // Read sheet inside the workbook by its name
+        Sheet loginSheet = loginWorkbook.getSheet(sheetName);
+
+        // Find number of rows in Excel file
+        int rowCount = loginSheet.getLastRowNum()-loginSheet.getFirstRowNum();
+
+        // Create a loop over all the rows of Excel file to read it
+
+        for (int i = 0; i < rowCount+1; i++) {
+
+            Row row = loginSheet.getRow(i);
+
+            // Create a loop to print cell values in a row
+
+            for (int j = 0; j < row.getLastCellNum(); j++) {
+
+                // Print Excel data in console
+
+                System.out.print(row.getCell(j).getStringCellValue() + " || ");
+
+            }
+
+            System.out.println();
+
+        }
+
+    }
+*/
 
 }
