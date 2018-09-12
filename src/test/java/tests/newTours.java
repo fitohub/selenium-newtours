398f3842a4642d639ada5c3bb3cb4ee68ddd57e0
fernandoito <fernando.ito.anywhere@gmail.com>
2018-09-12 19:05:34 -0400

    Data driver with loop

--- src/test/java/tests/newTours.java
+++ src/test/java/tests/newTours.java
@@ -49,8 +49,148 @@ public class newTours {
 
         // Call read file method of the class to read data
         //objExcelFile.readExcel(filePath, fileName, sheetName);
-        readExcel(filePath, fileName, sheetName);
+        //Workbook loginWorkbook = null;
+        //Sheet loginSheet = loginWorkbook.getSheet(sheetName);
 
+        //int qty = readExcel(filePath, fileName, sheetName, loginWorkbook, loginSheet);
+
+        // Create an object of File class to open xlsx file
+        File file = new File(filePath + "/" + fileName);
+
+        // Create an object of FileInputStream class to read excel file
+        FileInputStream inputStream = new FileInputStream(file);
+
+        Workbook loginWorkbook = null;
+
+        // Find the file extension by splitting file name in substring and getting only extension name
+
+        String fileExtensionName = fileName.substring(fileName.indexOf("."));
+
+        // Check condition if the file is xlsx file
+
+        if (fileExtensionName.equals(".xlsx")) {
+
+            // If it is xlsx file then create object of XSSFWorkbook class
+            loginWorkbook = new XSSFWorkbook(inputStream);
+
+        } else {
+
+            // if it is xls file then create object of HSSFWorkbook class
+            loginWorkbook = new HSSFWorkbook(inputStream);
+
+        }
+
+        // Read sheet inside the workbook by its name
+        Sheet loginSheet = loginWorkbook.getSheet(sheetName);
+        loginSheet = loginWorkbook.getSheet(sheetName);
+
+        // Find number of rows in Excel file
+        int rowCount = loginSheet.getLastRowNum()-loginSheet.getFirstRowNum();
+        HomeTest homeTest = new HomeTest();
+
+
+        // --- HOME - Login ------------------------------------------------ //
+        homeTest.homeLogin(driver);
+
+        // Click on Sign-In
+        driver.findElement(By.name("login")).click();
+
+
+        // Create a loop over all the rows of Excel file to read it
+        for (int i = 1; i < rowCount+1; i++) {
+
+            Row row = loginSheet.getRow(i);
+
+            // Create a loop to print cell values in a row
+
+            for (int j = 0; j < row.getLastCellNum(); j++) {
+
+                // Print Excel data in console
+
+                System.out.print(row.getCell(j).getStringCellValue() + " || ");
+
+            }
+
+            System.out.println();
+
+            // --- FLIGHT FINDER ----------------------------------------------- //
+
+            // Wait Flight Finder page appears
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("findFlights")));
+
+            FlightFinderTest flightFinderTest = new FlightFinderTest();
+
+            // --- FLIGHT FINDER - Flight Details --- //
+            flightFinderTest.flightDetails(driver);
+
+            // --- FLIGHT FINDER - Preferences --- //
+            flightFinderTest.flightPreferences(driver);
+
+            // Click Continue button
+            driver.findElement(By.name("findFlights")).click();
+
+
+            // --- SELECT FLIGHT ----------------------------------------------- //
+
+            // Wait SELECT FLIGHT page appears
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("reserveFlights")));
+
+            SelectFlightTest selectFlightTest = new SelectFlightTest();
+
+            // --- SELECT FLIGHT - Departure --- //
+            selectFlightTest.selectFlightDeparture(driver);
+
+            // --- SELECT FLIGHT - Return --- //
+            selectFlightTest.selectFlightReturn(driver);
+
+
+            // Click Continue button
+            driver.findElement(By.name("reserveFlights")).click();
+
+
+            // --- BOOK A FLIGHT ----------------------------------------------- //
+
+            // Wait BOOK A FLIGHT page appears
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("buyFlights")));
+
+            BookAFlightTest bookAFlightTest = new BookAFlightTest();
+
+            // --- BOOK A FLIGHT - Passengers --- //
+            bookAFlightTest.bookAFlightPassengers(driver);
+
+            // --- BOOK A FLIGHT - Credit Card --- //
+            bookAFlightTest.bookAFlightCreditCard(driver);
+
+            // --- BOOK A FLIGHT - Billing Address --- //
+            bookAFlightTest.bookAFlightBillingAddress(driver);
+
+            // --- BOOK A FLIGHT - Delivery Address --- //
+            bookAFlightTest.bookAFlightDeliveryAddress(driver);
+
+            // Click SECURE PURCHASE button
+            driver.findElement(By.name("buyFlights")).click();
+
+
+            // --- FLIGHT CONFIRMATION ----------------------------------------- //
+
+            // Wait FLIGHT CONFIRMATION page appears
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("img[src='/images/forms/Logout.gif']")));
+
+            FlightConfirmationTest flightConfirmationPage = new FlightConfirmationTest();
+
+            // Get confirmation text and check if is valid
+            flightConfirmationPage.flightConfirmation(wait);
+
+            if (i == rowCount+1) {
+                // Click LOG OUT button
+                driver.findElement(By.cssSelector("img[src='/images/forms/Logout.gif']")).click();
+            } else {
+                // Click BACK TO FLIGHTS button
+                driver.findElement(By.cssSelector("img[src='/images/forms/backtoflights.gif']")).click();
+            }
+            
+        }
+/*
         HomeTest homeTest = new HomeTest();
 
         // --- HOME - Login ------------------------------------------------ //
@@ -127,17 +267,18 @@ public class newTours {
 
         // Get confirmation text and check if is valid
         flightConfirmationPage.flightConfirmation(wait);
-
+*/
 
         // Click LOG OUT button
-        driver.findElement(By.cssSelector("img[src='/images/forms/Logout.gif']")).click();
+        //driver.findElement(By.cssSelector("img[src='/images/forms/Logout.gif']")).click();
 
         // Close the browser
         driver.quit();
 
     }
 
-    public static void readExcel(String filePath, String fileName, String sheetName) throws IOException {
+/*
+    public static Integer readExcel(String filePath, String fileName, String sheetName, Workbook loginWorkbook, Sheet loginSheet) throws IOException {
 
         // Create an object of File class to open xlsx file
         File file = new File(filePath + "/" + fileName);
@@ -145,7 +286,7 @@ public class newTours {
         // Create an object of FileInputStream class to read excel file
         FileInputStream inputStream = new FileInputStream(file);
 
-        Workbook loginWorkbook = null;
+        //Workbook loginWorkbook = null;
 
         // Find the file extension by splitting file name in substring and getting only extension name
 
@@ -166,13 +307,14 @@ public class newTours {
         }
 
         // Read sheet inside the workbook by its name
-        Sheet loginSheet = loginWorkbook.getSheet(sheetName);
+        //Sheet loginSheet = loginWorkbook.getSheet(sheetName);
+        loginSheet = loginWorkbook.getSheet(sheetName);
 
         // Find number of rows in Excel file
         int rowCount = loginSheet.getLastRowNum()-loginSheet.getFirstRowNum();
 
-        // Create a loop over all the rows of Excel file to read it
 
+        // Create a loop over all the rows of Excel file to read it
         for (int i = 0; i < rowCount+1; i++) {
 
             Row row = loginSheet.getRow(i);
@@ -191,6 +333,9 @@ public class newTours {
 
         }
 
+        return rowCount;
+
     }
+*/
 
 }
